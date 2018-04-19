fa8a4bb4d80cc848d48432729989f22c41fd71a0
PanagiotisFytas <pfytas@gmail.com>
2018-04-19 14:14:05 +0300

    Make sure that epmd has started

--- src/concuerror.hrl
+++ src/concuerror.hrl
@@ -174,6 +174,7 @@
               ['$1']}]))).
 %%------------------------------------------------------------------------------
 %% for parallel mode
+-define(epmd_not_running_response, "epmd: Cannot connect to local epmd\n").
 -define(initial_pid, "<0.110.0>").
 -define(pid_step, 15).
 -define(pid_number_of_tries, 200).
